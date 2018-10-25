e556e03fd2ca549df3ab20f65e9f07de84050f75
Massachusetts Association of Realtors <42622460+marealtors@users.noreply.github.com>
2018-10-25 16:04:27 -0400

    initial commit

---
+++ get_metadata.py
@@ -0,0 +1,12 @@
+import json
+import requests
+from pprint import pprint
+
+url = config.ramco_api_url
+payload = {
+    'key': config.ramco_api_key,
+    'Operation':'GetEntityMetadata',
+    'Entity':'contact'
+    } 
+metadata = requests.post(url,payload).json()
+print(metadata)
