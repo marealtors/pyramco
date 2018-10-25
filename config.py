58203e9b2456e2900bd8e3fcccc8d3a3106dc256
Massachusetts Association of Realtors <42622460+marealtors@users.noreply.github.com>
2018-10-25 15:21:33 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 # use this file to include any configuration info that will be used consistently / often in your scripts
 
+
 # RAMCO API endpoint url
 ramco_api_url = 'https://api.ramcoams.com/api/v2/'
+
 # your RAMCO API key
 ramco_api_key = 'YOUR_KEY_GOES_HERE'
